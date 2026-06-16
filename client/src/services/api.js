44ab63e51adfe037a89757f84425276ebbbbf943
leelahanuman <madasuleelahanuman@gmail.com>
2026-06-16 21:30:15 +0530

    fix: correct model path case

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api'
-   // baseURL: 'http://localhost:5000/api'//
+    //baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api'//
+    baseURL: 'http://localhost:5000/api'
 });
 
 // Every request  token will be auto add 
